760f8534e67bcc6eb59e536924ddc290be49945a
tuhamworld <52820977+tuhamworld@users.noreply.github.com>
2023-06-22 15:36:33 +0100

    stylings

--- js/script.js
+++ js/script.js
@@ -3,16 +3,25 @@ let btn = document.querySelector('.btn');
 let start = 0;
 
 let myImages = [
-    '../images/beautiful_scene.jpg',
-    '../images/caligraphy.jpg',
-    '../images/eid_mubarak.jpg',
-    '../images/lady_supplicating.jpg',
-    '../images/mosque.jpg',
-    '../images/quran.jpg',
-    '../images/ramadan_mubarak.jpg',
-    '../images/symbol.jpg'
+    // '../images/beautiful_scene.jpg',
+    // '../images/caligraphy.jpg',
+    // '../images/eid_mubarak.jpg',
+    // '../images/lady_supplicating.jpg',
+    // '../images/mosque.jpg',
+    // '../images/quran.jpg',
+    // '../images/ramadan_mubarak.jpg',
+    // '../images/symbol.jpg'
 
 ];
+
+myImages.push('../images/beautiful_scene.jpg')
+myImages.push('../images/caligraphy.jpg')
+myImages.push('../images/eid_mubarak.jpg')
+myImages.push('../images/lady_supplicating.jpg')
+myImages.push('../images/mosque.jpg')
+myImages.push('../images/quran.jpg')
+myImages.push('../images/ramadan_mubarak.jpg')
+myImages.push('../images/symbol.jpg')
 
 
 btn.addEventListener('click', changeImage, true);
@@ -27,14 +36,3 @@ function changeImage() {
     result.src = myImages[start];
 }
 result.src = myImages[start];
-
-
-
-// myImages.push('../images/beautiful_scene.jpg');
-// myImages.push('../images/caligraphy.jpg');
-// myImages.push('../images/eid_mubarak.jpg');
-// myImages.push('../images/lady_supplicating.jpg');
-// myImages.push('../images/mosque.jpg');
-// myImages.push('../images/quran.jpg');
-// myImages.push('../images/ramadan_mubarak.jpg');
-// myImages.push('../images/symbol.jpg');
